e7f97a6fa091fc7d84d30cdb42fdcffadfc53b07
cbrenner04 <cbrenner265@gmail.com>
2018-03-10 14:19:44 -0600

    Initial commit

---
+++ home/.bash_profile.d/prompt.sh
@@ -0,0 +1,7 @@
+Parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\[\033[0;38m\]\u \[\033[1;30m\]\D{%F %T}
+ \[\033[0;32m\]\$(~/.rvm/bin/rvm-prompt) \[\033[0;33m\]\$(python --version) \[\033[0;36m\]node-\$(node --version) \[\033[0;35m\]\w
+ \[\033[0;31m\]\$(Parse_git_branch)\[\033[00m\] $ "
---
+++ home/.bash_profile.d/qo-automation-functions.sh
@@ -0,0 +1,27 @@
+runPriorityTests() {
+  pip3 install -U .
+  python3 -u suites/Priority.py -env "$1" -brow "$2" -test "$3" 2>&1 | tee Results/output$(date +%s).txt
+}
+
+runFunctionalityTests() {
+  TEST_ENV="$1"
+  TEST_BROWSER="$2"
+  TEST_SUITE="$3"
+  shift 3
+  pip3 install -U .
+  python3 -u suites/Functionality.py -env "$TEST_ENV" -brow "$TEST_BROWSER" -test "$TEST_SUITE" -case "$@" 2>&1 | tee Results/output$(date +%s).txt
+}
+
+runPrioritySauce() {
+  pip3 install . -U
+  python3 -u suites/Priority.py -env "$1" -brow sauce -sb "$2" -test "$3" 2>&1 | tee Results/output$(date +%s).txt
+}
+
+runFunctionalitySauce() {
+  TEST_ENV="$1"
+  TEST_BROWSER="$2"
+  TEST_SUITE="$3"
+  shift 3
+  pip3 install . -U
+  python3 -u suites/Functionality.py -env "$TEST_ENV" -brow sauce -sb "$TEST_BROWSER" -test "$TEST_SUITE" -case "$@" 2>&1 | tee Results/output$(date +%s).txt
+}
